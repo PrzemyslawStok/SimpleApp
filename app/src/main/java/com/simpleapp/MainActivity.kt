3e8eca05732d96e53550649736dd2a0c9dc56fb7
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-10-22 17:42:21 +0200

    cd ..

--- app/src/main/java/com/simpleapp/MainActivity.kt
+++ app/src/main/java/com/simpleapp/MainActivity.kt
@@ -4,20 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.simpleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var i = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /*setContentView(R.layout.activity_main)
 
         val textView1:TextView = findViewById(R.id.textView1)
         val startButton: Button = findViewById(R.id.startButton)
 
-        textView1.setText("TextView")
 
         startButton.setOnClickListener {
-            textView1.setText("Text1")
+            textView1.setText("$i")
+            i++
         }
+        */
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView1.setText("TextView")
 
         add(10.0,20.0)
     }
