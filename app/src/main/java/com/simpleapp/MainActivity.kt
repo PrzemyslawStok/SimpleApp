21473de1712559917032461ed99d582c0934b781
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-10-22 17:14:59 +0200

    cd ..

--- app/src/main/java/com/simpleapp/MainActivity.kt
+++ app/src/main/java/com/simpleapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView1:TextView = findViewById(R.id.textView1)
+        val startButton: Button = findViewById(R.id.startButton)
+
+        textView1.setText("TextView")
+
+        startButton.setOnClickListener {
+            textView1.setText("Text1")
+        }
+
+        add(10.0,20.0)
+    }
+
+    fun add(x: Double, y: Double):Double{
+        return x+y
     }
 }
