342f40590754b377dce05bc273f263d519fe454f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-12-17 17:06:45 +0100

    cd..

--- app/src/main/java/com/simpleapp/NextActivity.kt
+++ app/src/main/java/com/simpleapp/NextActivity.kt
@@ -2,10 +2,19 @@ package com.simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simpleapp.databinding.ActivityNextLayoutBinding
 
 class NextActivity : AppCompatActivity() {
+    val number = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_next_layout)
+        //setContentView(R.layout.activity_next_layout)
+
+        val binding = ActivityNextLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainTextView.setText("${number}")
+
     }
 }
