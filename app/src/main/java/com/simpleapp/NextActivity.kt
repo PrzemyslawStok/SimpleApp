324c5737c9b8f5a3b118dec554bb3db983942472
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-12-17 17:34:13 +0100

    §

--- app/src/main/java/com/simpleapp/NextActivity.kt
+++ app/src/main/java/com/simpleapp/NextActivity.kt
@@ -2,14 +2,18 @@ package com.simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import com.simpleapp.databinding.ActivityNextLayoutBinding
 
 class NextActivity : AppCompatActivity() {
+    var initNumber = 101
     var number = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_next_layout)
+
+        number = initNumber
 
         val binding = ActivityNextLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,5 +25,11 @@ class NextActivity : AppCompatActivity() {
             binding.mainTextView.setText("${number}")
         }
 
+    }
+
+    fun restartGame(){
+        if(number==initNumber){
+
+        }
     }
 }
