b4526ab4b5840cbd6ffedee9e7f6f8801abcb12a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-07 17:14:29 +0100

    §

--- app/src/main/java/com/simpleapp/NextActivity.kt
+++ app/src/main/java/com/simpleapp/NextActivity.kt
@@ -2,6 +2,7 @@ package com.simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.simpleapp.databinding.ActivityNextLayoutBinding
@@ -10,6 +11,9 @@ import kotlin.random.Random
 class NextActivity : AppCompatActivity() {
     var initNumber = 0
     var number = 0
+    var trialsNumber = 0
+
+    var mainNumberText: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,18 +21,17 @@ class NextActivity : AppCompatActivity() {
 
         //number = initNumber
 
-        while(number==0){
-            number = Random.nextInt(-100,100)
-        }
+        newGame()
 
         val binding = ActivityNextLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mainNumberText = binding.mainTextView
 
         binding.mainTextView.setText("${number}")
 
         binding.buttonLeft.setOnClickListener {
             number+=9
-            binding.mainTextView.setText("${number}")
 
             restartGame()
         }
@@ -36,30 +39,39 @@ class NextActivity : AppCompatActivity() {
 
         binding.buttonRight.setOnClickListener {
             number-=7
-            binding.mainTextView.setText("${number}")
 
             restartGame()
         }
 
         binding.buttonUp.setOnClickListener {
             number+=3
-            binding.mainTextView.setText("${number}")
 
             restartGame()
         }
 
         binding.buttonDown.setOnClickListener {
             number-=5
-            binding.mainTextView.setText("${number}")
 
             restartGame()
         }
 
     }
 
+    fun newGame(){
+        while(number==0){
+            number = Random.nextInt(-100,100)
+        }
+        trialsNumber = 0
+    }
+
     fun restartGame(){
+        trialsNumber++
+
+        mainNumberText?.setText("${number}")
+
         if(number==initNumber){
-            Toast.makeText(this,"Gra zakończona po ...",Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Gra zakończona po $trialsNumber próbach",Toast.LENGTH_LONG).show()
+            newGame()
         }
     }
 }
