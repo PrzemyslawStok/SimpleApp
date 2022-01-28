f766197205ddcb3ba39b2c57e53295e726ad9d91
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 17:27:32 +0100

    cd ..

--- app/src/main/java/com/simpleapp/Game2Activity.kt
+++ app/src/main/java/com/simpleapp/Game2Activity.kt
@@ -3,6 +3,10 @@ package com.simpleapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.LinearLayoutCompat
 import com.simpleapp.databinding.ActivityGame1Binding
 import com.simpleapp.databinding.ActivityGame2Binding
@@ -15,8 +19,26 @@ class Game2Activity : AppCompatActivity() {
         setContentView(binding.root)
 
         val mainLayout = binding.mainLayout
-        mainLayout.setBackgroundColor(Color.GRAY)
+
+        //mainLayout.setBackgroundColor(Color.GRAY)
         mainLayout.removeAllViews()
+
+
+        for (i in 1..10) {
+            val view = View(this)
+            if (i % 2 == 0)
+                view.setBackgroundColor(Color.CYAN)
+            else
+                view.setBackgroundColor(Color.GRAY)
+
+            view.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT, 1.0f
+            )
+
+            mainLayout.addView(view)
+        }
+
 
     }
 }
