75e6033c3a2bc2cafd66e916ed6f2190e048d9ba
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 17:07:39 +0100

    cd ..

--- app/src/main/java/com/simpleapp/Game2Activity.kt
+++ app/src/main/java/com/simpleapp/Game2Activity.kt
@@ -1,11 +1,21 @@
 package com.simpleapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.LinearLayoutCompat
+import com.simpleapp.databinding.ActivityGame1Binding
+import com.simpleapp.databinding.ActivityGame2Binding
 
 class Game2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_2)
+
+        val binding = ActivityGame2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mainLayout = binding.mainLayout
+        mainLayout.setBackgroundColor(Color.GRAY)
+
     }
 }
