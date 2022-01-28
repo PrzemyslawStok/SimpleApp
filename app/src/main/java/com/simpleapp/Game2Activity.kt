20a2fa73a42725c4620fd561f4e6c2f44dccc3f3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 17:47:08 +0100

    cd ..

--- app/src/main/java/com/simpleapp/Game2Activity.kt
+++ app/src/main/java/com/simpleapp/Game2Activity.kt
@@ -1,15 +1,10 @@
 package com.simpleapp
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.LinearLayoutCompat
-import com.simpleapp.databinding.ActivityGame1Binding
 import com.simpleapp.databinding.ActivityGame2Binding
 
 class Game2Activity : AppCompatActivity() {
